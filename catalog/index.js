16de603b60415db7b67043900a66d6c6cae37426
Matej Kamenicky <matt.kamenicky@gmail.com>
2020-05-11 16:26:56 +0200

    edit theme.js

--- catalog/index.js
+++ catalog/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Catalog, pageLoader } from "catalog";
-import theme from "./Theme";
+import theme from "./theme";
 
 const pages = [
   {
