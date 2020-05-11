97bd49a8c467c0ccf9649ef81a734f1e915e16bb
Matej Kamenicky <matt.kamenicky@gmail.com>
2020-05-11 16:22:15 +0200

    style.js add

---
+++ catalog/theme.js
@@ -0,0 +1,66 @@
+/* eslint-disable key-spacing */
+
+export default {
+    // Colors
+    background: "#282124",
+    textColor: "#fff",
+    codeColor: "#00263E",
+    linkColor: "#FF5555",
+  
+    // NavigationBar background color, but also sometimes used as a foreground
+    // or border color.
+    lightColor: "#D6D6D6",
+  
+    // Used in PageHeader
+    pageHeadingBackground: "#282124",
+    pageHeadingTextColor: "#ffd900",
+  
+    // Used in Menu and PageHeader to make sure the top parts have
+    // the same height.
+    pageHeadingHeight: 200,
+  
+    // Used for navigation bar
+    navBarBackground: "#282124",
+    navBarTextColor: "#fff",
+  
+    // Used in ResponsiveTabs (tab text), Download specimen (title text).
+    // Typography: headings.
+    brandColor: "#003B5C",
+  
+    sidebarColor: "#ffd900",
+    sidebarColorText: "#282124",
+    sidebarColorTextActive: "#fff",
+    sidebarColorLine: "#282124",
+    sidebarColorHeading: "#282124",
+  
+    // Used in the html, react, and image specimens.
+    bgLight: "#F2F2F2",
+    bgDark: "#333333",
+  
+    // Keys appear to be PrismJS token types.
+    codeStyles: {
+      tag: { color: "#FF5555" },
+      punctuation: { color: "#535353" },
+      script: { color: "#3F7397" },
+      function: { color: "#FF5555" },
+      keyword: { color: "#3F7397" },
+      string: { color: "#00263E" }
+    },
+  
+    // Patterns
+    checkerboardPatternLight:
+      "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAAAAACoWZBhAAAAF0lEQVQI12P4BAI/QICBFCaYBPNJYQIAkUZftTbC4sIAAAAASUVORK5CYII=",
+    checkerboardPatternDark:
+      "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAAAAACoWZBhAAAAFklEQVQI12NQBQF2EGAghQkmwXxSmADZJQiZ2ZZ46gAAAABJRU5ErkJggg==",
+  
+    // Fonts
+    fontFamily: "'Poppins'",
+    fontHeading: "'Poppins'",
+    fontMono: "'Poppins'",
+  
+    // Base font size in pixels.
+    baseFontSize: 16,
+  
+    // Modular scale ratio that is used to figure out all the different font sizes
+    msRatio: 1.2
+  };
